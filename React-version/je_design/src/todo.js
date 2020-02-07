2b54a6656f4e358ae1b295f3e60f04ee283187d4
ThsBzs <t.brouzes@gmail.com>
2020-02-07 14:12:09 +0100

    Create todo.js

---
+++ React-version/je_design/src/todo.js
@@ -0,0 +1,7 @@
+/* TODO sur version lourde : 
+    - Ajouter un objet 'link' dans chaque élément de mobilier et des produits, pour que le composant containerHome récupère ça à la volée sur les images
+    - Trouver un moyen de récupérer les images, uniquement la première du lot à chaque fois, pour le containerHome. Attention, il y aura un problème avec Sapa, qui comporte deux objtes mobilier
+    - Reprendre le carousel Abyss
+    - Reprendre le menu au niveaud e l'affichage réduit, la bascule ne se fait pas assez rapidement
+
+*/
