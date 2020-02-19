c0841751a471b7f4e050505ae074cda8f03d724e
ThsBzs <t.brouzes@gmail.com>
2020-02-19 13:51:29 +0100

    Update global, mise en ligne

--- React-version/je_design/src/App.js
+++ React-version/je_design/src/App.js
@@ -24,8 +24,8 @@ class App extends Component {
   render () {
     return (
       <Fragment>
-        <Nav />
         <BrowserRouter>
+        <Nav />
             <Switch>
               {/*ACCUEIL*/}
               <Route exact path='/' component={Home}/>
--- React-version/je_design/src/todo.js
+++
@@ -1,3 +0,0 @@
-/* TODO sur version lourde : 
-    - reprendre le CSS de burgernav pour la checkbox -> il faut que l'utilisateur de smartphone puisse cliquer dessus pour dérouler le menu
-*/
